bd1e3dfe2c5dfe29fc673705d392ecce4e3b7544
Alexander Zuev <zuev08@gmail.com>
2016-05-20 15:35:24 +0300

    Fix Rune view drawing
    Add new rune types
    Change colors

--- app/src/main/java/com/xoul/ru/magone/view/Rune.java
+++ app/src/main/java/com/xoul/ru/magone/view/Rune.java
@@ -10,40 +10,52 @@ import android.view.View;
 import com.xoul.ru.magone.R;
 
 public class Rune extends View {
+    private static final float BORDER_MULTIPLER = 1 / 10f;
     private Paint paint;
+    private RuneStyle style;
 
     public Rune(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Rune, 0, 0);
         int runeIndex = typedArray.getInteger(R.styleable.Rune_type, 0);
-        RuneStyle style = RuneStyle.values()[runeIndex];
+        style = RuneStyle.values()[runeIndex];
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(style.getColor());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(50, 50, 50, paint);
+        int w = getWidth();
+        int h = getHeight();
+        float r = (h < w) ? h : w; // min(h, w);
+        r /= 2f;
+        paint.setColor(style.getBorderColor());
+        canvas.drawCircle(w / 2f, h / 2f, r, paint);
+        float r2 = r * (1 - BORDER_MULTIPLER);
+        paint.setColor(style.getColor());
+        canvas.drawCircle(w / 2f, h / 2f, r2, paint);
     }
 
+    // using order of attrs.xml type enum
     private enum RuneStyle {
-        FIRE(0xffff1111, 0xffaaaa00),
-        WATER(0xff0000aa, 0xff00aaaa);
+        FIRE(0xffff0000, 0xffaa0000),
+        WATER(0xff0000ff, 0xff0000aa),
+        LIFE(0xff00ff00, 0xff00aa00),
+        DEATH(0xff000000, 0xff555555);
 
         private int color;
-        private int boarderColor;
+        private int borderColor;
 
-        RuneStyle(int color, int boarderColor) {
+        RuneStyle(int color, int borderColor) {
             this.color = color;
-            this.boarderColor = boarderColor;
+            this.borderColor = borderColor;
         }
 
         public int getColor() {
             return color;
         }
 
-        public int getBoarderColor() {
-            return boarderColor;
+        public int getBorderColor() {
+            return borderColor;
         }
     }
 }
