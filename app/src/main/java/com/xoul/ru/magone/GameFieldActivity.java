1b32af96d96ff28711b8ce8e9d6c167a005d08d9
Alexander Zuev <zuev08@gmail.com>
2016-05-23 23:16:37 +0300

    Add Unit, implement UnitField

--- app/src/main/java/com/xoul/ru/magone/GameFieldActivity.java
+++ app/src/main/java/com/xoul/ru/magone/GameFieldActivity.java
@@ -7,9 +7,12 @@ import com.xoul.ru.magone.view.ManaBar;
 import com.xoul.ru.magone.view.Rune;
 import com.xoul.ru.magone.view.RuneField;
 import com.xoul.ru.magone.view.RuneField.OnRuneClickedListener;
+import com.xoul.ru.magone.view.Unit;
+import com.xoul.ru.magone.view.UnitField;
 
 public class GameFieldActivity extends Activity implements OnRuneClickedListener {
     private ManaBar manaBar;
+    private UnitField unitField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class GameFieldActivity extends Activity implements OnRuneClickedListener
         manaBar = (ManaBar) findViewById(R.id.manaBar);
         RuneField field = (RuneField) findViewById(R.id.rune_field);
         field.setOnRuneClickedListener(this);
+        unitField = (UnitField) findViewById(R.id.unitField);
     }
 
     @Override
@@ -28,5 +32,19 @@ public class GameFieldActivity extends Activity implements OnRuneClickedListener
             mp = manaBar.getMaxValue();
         }
         manaBar.setValue(mp);
+        switch (runeStyle) {
+            case DEATH:
+                unitField.removeAllUnits();
+                break;
+            case LIFE:
+                unitField.addUnit(new Unit(this), UnitField.Slot.HERO);
+                break;
+            case FIRE:
+                unitField.addUnit(new Unit(this), UnitField.Slot.WARRIOR);
+                break;
+            case WATER:
+                unitField.addUnit(new Unit(this), UnitField.Slot.SUPPORT);
+                break;
+        }
     }
 }
--- app/src/main/java/com/xoul/ru/magone/view/UnitField.java
+++ app/src/main/java/com/xoul/ru/magone/view/UnitField.java
@@ -1,20 +1,84 @@
 package com.xoul.ru.magone.view;
 
 import android.content.Context;
-import android.view.View;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public class UnitField extends View {
-//    Unit Warrior;
-//    Unit Entity;
-//    Unit provoker;
+public class UnitField extends LinearLayout {
+    private static final int UNIT_WIDTH_DP = 100;
+    private static final int UNIT_MARGIN_DP = 5;
+    private LayoutParams layoutParams;
+
+    private Unit[] units;
 
     public UnitField(Context context) {
         super(context);
+        initLayout();
+        initUnits(context);
     }
 
-//    public UnitField() {
-//        Warrior = new Unit();
-//        Entity = null;
-//        provoker = null;
-//    }
+    public UnitField(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initLayout();
+        initUnits(context);
+    }
+
+    private void initLayout() {
+        units = new Unit[Slot.values().length];
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER);
+
+        int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, UNIT_WIDTH_DP, getResources().getDisplayMetrics());
+        int margin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, UNIT_MARGIN_DP, getResources().getDisplayMetrics());
+        layoutParams = new LayoutParams(height, ViewGroup.LayoutParams.MATCH_PARENT);
+        layoutParams.setMargins(margin, margin, margin, margin);
+    }
+
+    private void initUnits(Context context) {
+        addUnit(new Unit(context), Slot.HERO);
+    }
+
+    public void addUnit(Unit unit, Slot slot) {
+        if (units[slot.ordinal()] != null) {
+            removeUnit(slot);
+        }
+        units[slot.ordinal()] = unit;
+        removeAllUnitViews();
+        for (Unit item : units) {
+            if (item != null) {
+                addView(item, layoutParams);
+            }
+        }
+    }
+
+    public void removeUnit(Slot slot) {
+        removeView(units[slot.ordinal()]);
+        units[slot.ordinal()] = null;
+    }
+
+    public void removeAllUnits() {
+        for (int i = 0; i < units.length; i++) {
+            if (units[i] != null) {
+                removeView(units[i]);
+                units[i] = null;
+            }
+        }
+    }
+
+    private void removeAllUnitViews() {
+        for (Unit unit : units) {
+            if (unit != null) {
+                removeView(unit);
+            }
+        }
+    }
+
+    public enum Slot {
+        WARRIOR,
+        HERO,
+        SUPPORT
+    }
 }
